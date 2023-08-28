f99e9581cdc9eab8a9d1fe1b4ab666695e76d07b
John Whitman <john@s2sq.com>
2023-08-28 10:39:20 -0400

    Renames project directory to config.

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "hackduke2023backend.apps.weather",
+    "weather",
 ]
 
 MIDDLEWARE = [
