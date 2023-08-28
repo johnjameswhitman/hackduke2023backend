b075648812589abf4bc31cf1613b3f31dcd61be1
John Whitman <john@s2sq.com>
2023-08-28 09:46:58 -0400

    Rearranges requirements.

--- scripts/compile_requirements.sh
+++ scripts/compile_requirements.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-pip-compile --resolver=backtracking -o requirements.txt requirements.in
+pip-compile --resolver=backtracking -o requirements/deployment.txt requirements/deployment.in
+pip-compile --resolver=backtracking -o requirements/development.txt requirements/development.in
