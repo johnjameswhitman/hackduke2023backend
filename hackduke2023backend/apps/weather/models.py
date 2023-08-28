6d09673e592608cd8d5e8168165b6459acca484c
John Whitman <john@s2sq.com>
2023-08-27 20:54:26 -0400

    Initial weather data model.

--- hackduke2023backend/apps/weather/models.py
+++ hackduke2023backend/apps/weather/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class WeatherAlertConfig(models.Model):
+    """Configuration for an alert that a user is interested in."""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    state = models.CharField(max_length=2)
