9789ff7c532cf3c09f1140286cb9a12cb57f1edb
John Whitman <jwhitman@galileo.io>
2023-08-29 10:42:25 -0400

    Pins requirements to public index.

--- scripts/compile_requirements.sh
+++ scripts/compile_requirements.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-pip-compile --resolver=backtracking -o requirements/deployment.txt requirements/deployment.in
-pip-compile --resolver=backtracking -o requirements/development.txt requirements/development.in
+pip-compile --resolver=backtracking --index-url=https://pypi.org/simple -o requirements/deployment.txt requirements/deployment.in
+pip-compile --resolver=backtracking --index-url=https://pypi.org/simple -o requirements/development.txt requirements/development.in
