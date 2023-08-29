cc68e54a5c65f1fafbe1a36754ca16801482eeec
John Whitman <john@s2sq.com>
2023-08-28 20:36:05 -0400

    Returns 401 when user creds not found.

--- auth/views.py
+++ auth/views.py
@@ -3,6 +3,7 @@
 Borrowed from: https://github.com/vitalik/django-ninja/issues/45#issuecomment-1049829818
 """
 from django.contrib.auth import authenticate
+from django.http import HttpResponse
 from ninja import Router, Schema
 from rest_framework_simplejwt.tokens import RefreshToken
 
@@ -22,6 +23,10 @@ class LoginResponse(Schema):
 @router.post("/login", response=LoginResponse, auth=None)
 def login(request, auth: LoginRequest):
     user = authenticate(**auth.dict())
+
+    if not user:
+        return HttpResponse("Unauthorized", status=401)
+
     refresh = RefreshToken.for_user(user)
 
     return LoginResponse(
