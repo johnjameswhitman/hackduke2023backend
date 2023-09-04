c16c069fdeeb17cccaae8b3a59cd8921b8a6c2c5
John Whitman <john@s2sq.com>
2023-09-04 16:25:04 -0400

    Adds solution for modeling lab.

--- weather/models.py
+++ weather/models.py
@@ -2,8 +2,23 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+class Severity(models.TextChoices):
+    EXTREME = "Extreme"
+    SEVERE = "Severe"
+    MODERATE = "Moderate"
+    MINOR = "Minor"
+    UNKNOWN = "Unknown"
+
+
 class WeatherAlertConfig(models.Model):
     """Configuration for an alert that a user is interested in."""
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     state_abbreviation = models.CharField(max_length=2)
+    severity = models.CharField(
+        max_length=10,
+        choices=Severity.choices,
+        # Below options allow users to omit a severity if they want everything.
+        blank=True,
+        null=True,
+    )
