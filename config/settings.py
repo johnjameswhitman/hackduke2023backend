e2eb69c8345816f47a135e4461f6aacd1aae93a5
John Whitman <john@s2sq.com>
2023-09-04 13:04:08 -0400

    Fixes staticfiles storage.

--- config/settings.py
+++ config/settings.py
@@ -102,6 +102,9 @@ STORAGES = {
     "default": {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
 }
 
 # Logging
